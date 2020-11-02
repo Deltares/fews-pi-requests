a7964a063547c660e9d3bd1fe96df9c8fe0a0763
Werner Kramer <werner.kramer@gmail.com>
2020-11-02 13:04:25 +0100

    Set correct library and file name

--- webpack.config.js
+++ webpack.config.js
@@ -6,9 +6,9 @@ module.exports = {
   entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    library: 'time-series',
+    library: 'fews-pi-requests',
     libraryTarget: 'umd',
-    filename: 'time-series.umd.js',
+    filename: 'fews-pi-requests.umd.js',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
