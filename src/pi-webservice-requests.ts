8895833490911862a40ac12568885c269df97eed
Werner Kramer <werner.kramer@gmail.com>
2020-11-13 10:17:52 +0100

    Update type information and add method documentation

--- src/pi-webservice-requests.ts
+++ src/pi-webservice-requests.ts
@@ -16,7 +16,7 @@ import {
 import { requestJson } from './utils/requests'
 import { filterToParams } from './utils/filter'
 
-const attributesForKey: { [key: string]: any } = {
+const attributesForKey: { [key: string]: string } = {
   parameterIds: 'long_name',
 }
 
@@ -24,10 +24,22 @@ export class PiWebserviceProvider {
   baseUrl: URL
   readonly API_ENDPOINT = '/FewsWebServices/rest/fewspiservice/v1'
 
+/**
+ * Constructor for PiWebserviceProvider
+ *
+ * @param url the base url where the PI servive is available
+ */
   constructor(url: string) {
     this.baseUrl = new URL('', url)
   }
 
+
+/**
+ * Request parameters
+ *
+ * @param filter an object with request query parameters
+ * @returns Parameters PI API response
+ */
   getParameters(filter: ParametersFilter): Promise<ParametersResponse> {
     const queryParameters = filterToParams(filter)
     const url = this.parametersUrl(queryParameters)
@@ -35,6 +47,12 @@ export class PiWebserviceProvider {
     return promise as Promise<ParametersResponse>
   }
 
+  /**
+ * Request locations
+ *
+ * @param filter an object with request query parameters
+ * @returns Locations PI API response
+ */
   getLocations(filter: LocationsFilter): Promise<LocationsResponse> {
     const queryParameters = filterToParams(filter)
     const url = this.locationsUrl(queryParameters)
@@ -42,6 +60,12 @@ export class PiWebserviceProvider {
     return promise as Promise<LocationsResponse>
   }
 
+  /**
+ * Request attributes
+ *
+ * @param filter an object with request query parameters
+ * @returns Attributes PI API response
+ */
   getAttributes(filter: AttributesFilter): Promise<AttributesResponse> {
     const queryParameters = filterToParams(filter)
     const url = this.attributesUrl(queryParameters)
@@ -49,10 +73,16 @@ export class PiWebserviceProvider {
     return promise as Promise<AttributesResponse>
   }
 
+  /**
+ * Request external forecasts
+ *
+ * @param filter an object with request query parameters
+ * @returns External Forecasts PI API response
+ */
   getExternalForecasts(
     filter: ExternalForecastsFilter
   ): Promise<ExternalForecastsResponse> {
-    const mappedFilter: { [key: string]: any } = {}
+    const mappedFilter: { [key: string]: unknown } = {}
     for (const [key, value] of Object.entries(filter)) {
       if (key in Object.keys(attributesForKey)) {
         mappedFilter[attributesForKey[key]] = value
@@ -60,7 +90,7 @@ export class PiWebserviceProvider {
         mappedFilter[key] = value
       }
     }
-    const defaults: any = {
+    const defaults: ExternalForecastsFilter = {
       documentFormat: DocumentFormat.PI_JSON,
     }
     const filterWithDefaults = { ...mappedFilter, ...defaults }
@@ -70,8 +100,15 @@ export class PiWebserviceProvider {
     return promise as Promise<ExternalForecastsResponse>
   }
 
+
+/**
+ * Request Time Series
+ *
+ * @param filter an object with request query parameters
+ * @returns Time Series PI API response
+ */
   getTimeSeries(filter: TimeSeriesFilter): Promise<TimeSeriesResponse> {
-    const defaults: any = {
+    const defaults: TimeSeriesFilter = {
       documentFormat: DocumentFormat.PI_JSON,
     }
     const filterWithDefaults = { ...defaults, ...filter }
@@ -81,6 +118,12 @@ export class PiWebserviceProvider {
     return promise as Promise<TimeSeriesResponse>
   }
 
+/**
+ * Construct URL for locations request
+ *
+ * @param queryParameters query string
+ * @returns complete url for making a request
+ */
   locationsUrl(queryParameters: string): URL {
     return new URL(
       `${this.baseUrl.pathname}${this.API_ENDPOINT}/archive/locations${queryParameters}`,
@@ -88,6 +131,12 @@ export class PiWebserviceProvider {
     )
   }
 
+/**
+ * Construct URL for parameters request
+ *
+ * @param queryParameters query string
+ * @returns complete url for making a request
+ */
   parametersUrl(queryParameters: string): URL {
     return new URL(
       `${this.baseUrl.pathname}${this.API_ENDPOINT}/archive/parameters${queryParameters}`,
@@ -95,6 +144,12 @@ export class PiWebserviceProvider {
     )
   }
 
+/**
+ * Construct URL for attribute request
+ *
+ * @param queryParameters query string
+ * @returns complete url for making a request
+ */
   attributesUrl(queryParameters: string): URL {
     return new URL(
       `${this.baseUrl.pathname}${this.API_ENDPOINT}/archive/attributes${queryParameters}`,
@@ -102,6 +157,12 @@ export class PiWebserviceProvider {
     )
   }
 
+/**
+ * Construct URL for external forecast request
+ *
+ * @param queryParameters query string
+ * @returns complete url for making a request
+ */
   externalForecastsUrl(queryParameters: string): URL {
     return new URL(
       `${this.baseUrl.pathname}${this.API_ENDPOINT}/archive/netcdfstorageforecasts${queryParameters}`,
@@ -109,6 +170,12 @@ export class PiWebserviceProvider {
     )
   }
 
+/**
+ * Construct URL for time series request
+ *
+ * @param queryParameters query string
+ * @returns complete url for making a request
+ */
   timeSeriesUrl(queryParameters: string): URL {
     const url = new URL(
       `${this.baseUrl.pathname}${this.API_ENDPOINT}/timeseries${queryParameters}`,
