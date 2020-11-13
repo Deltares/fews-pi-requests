bd9e2276c71a833e2f8ef146cd0904475cb32e6c
Werner Kramer <werner.kramer@gmail.com>
2020-11-13 10:40:23 +0100

    Add type definitions

--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,6 +1,6 @@
-function filterArgToStrings(key: string, value: any): string[] {
+function filterArgToStrings(key: string, value: any ): string[] {
     const result: string[] = []
-    if (value.constructor === Array) {
+    if (value instanceof Array) {
         for (const item of value) {
             result.push(`${key}=${encodeURIComponent(item)}`)
         }
@@ -10,8 +10,8 @@ function filterArgToStrings(key: string, value: any): string[] {
     return result
 }
 
-export function filterToParams(filter: Record < string, any > ): string {
-    const filterArgs: any[] = []
+export function filterToParams(filter: Record < string, object > ): string {
+    const filterArgs: string[] = []
     for (const [key, value] of Object.entries(filter)) {
         if (value === undefined) continue
         const strings = filterArgToStrings(key, value)
