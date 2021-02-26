c8fd3d3e2427d1a5672137cde7405e70f66e503a
Werner Kramer <werner.kramer@vortech.nl>
2021-02-26 13:28:00 +0100

    Refactor splitUrl

--- src/utils/requests.ts
+++ src/utils/requests.ts
@@ -21,23 +21,24 @@ export function requestJson<T>(url: URL): Promise<T> {
     return promise as Promise<T>
 }
 
-export function splitUrl(url: URL, maxLength = 2000, byKey?: string): URL[] {
-    if ( url.toString().length <= maxLength ) return [url]
+function mostFrequentParameter(url: URL): string {
     const duplicateEntries = duplicates(Array.from(url.searchParams.keys()))
     const keys = Array.from(duplicateEntries.keys())
-    const baseUrl = new URL(url.toString())
-    // delete duplicate keys from url
-    let split = byKey
-    if ( split === undefined) {
-        let max = 0
-        split = keys[0]
-        for (const [key, value] of duplicateEntries) {
-            if ( value > max) {
-                max = value
-                split = key
-            }
+    let max = 0
+    let split = keys[0]
+    for (const [key, value] of duplicateEntries) {
+        if ( value > max) {
+            max = value
+            split = key
         }
     }
+    return split
+}
+
+export function splitUrl(url: URL, maxLength = 2000, byKey?: string): URL[] {
+    if ( url.toString().length <= maxLength ) return [url]
+    const baseUrl = new URL(url.toString())
+    const split = byKey !== undefined ? byKey : mostFrequentParameter(url)
     baseUrl.searchParams.delete(split)
     const urls: URL[] = []
     let newUrl = new URL(baseUrl.toString())
