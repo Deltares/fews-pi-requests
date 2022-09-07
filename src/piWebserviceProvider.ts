cbad6ab7deed0a8e6da8f950a23a83de6dc83225
Cees Voesenek <cees.voesenek@vortech.nl>
2022-09-07 16:53:38 +0200

    Let URL getters accept a filter instead of query parameters
    Since these functions are exposed, they are much more useful if they
    accept a filter, similar to the fetch methods.

--- src/piWebserviceProvider.ts
+++ src/piWebserviceProvider.ts
@@ -56,8 +56,7 @@ export class PiWebserviceProvider {
      * @returns Parameters PI API response
      */
     async getParameters(filter: ParametersFilter): Promise<ParametersResponse> {
-        const queryParameters = filterToParams(filter);
-        const url = this.parametersUrl(queryParameters);
+        const url = this.parametersUrl(filter);
         const res = await this.webservice.getData<ParametersResponse>(url.toString());
         return res.data;
     }
@@ -69,8 +68,7 @@ export class PiWebserviceProvider {
      * @returns Locations PI API response
      */
     async getLocations(filter: LocationsFilter): Promise<LocationsResponse> {
-        const queryParameters = filterToParams(filter);
-        const url = this.locationsUrl(queryParameters, false);
+        const url = this.locationsUrl(filter, false);
         const res = await this.webservice.getData<LocationsResponse>(url.toString());
         return res.data;
     }
@@ -82,8 +80,7 @@ export class PiWebserviceProvider {
      * @returns Locations PI API response
      */
     async getArchiveLocations(filter: ArchiveLocationsFilter): Promise<LocationsResponse> {
-        const queryParameters = filterToParams(filter);
-        const url = this.locationsUrl(queryParameters, true);
+        const url = this.locationsUrl(filter, true);
         const res = await this.webservice.getData<LocationsResponse>(url.toString());
         return res.data;
     }
@@ -95,8 +92,7 @@ export class PiWebserviceProvider {
      * @returns Attributes PI API response
      */
     async getAttributes(filter: AttributesFilter): Promise<AttributesResponse> {
-        const queryParameters = filterToParams(filter);
-        const url = this.attributesUrl(queryParameters);
+        const url = this.attributesUrl(filter);
         const res = await this.webservice.getData<AttributesResponse>(url.toString());
         return res.data;
     }
@@ -120,8 +116,7 @@ export class PiWebserviceProvider {
             documentFormat: DocumentFormat.PI_JSON,
         }
         const filterWithDefaults = {...mappedFilter, ...defaults}
-        const queryParameters = filterToParams(filterWithDefaults)
-        const url = this.externalForecastsUrl(queryParameters)
+        const url = this.externalForecastsUrl(filterWithDefaults)
         const res = await this.webservice.getData<ExternalForecastsResponse>(url.toString());
         return res.data;
     }
@@ -138,8 +133,7 @@ export class PiWebserviceProvider {
             documentFormat: DocumentFormat.PI_JSON,
         }
         const filterWithDefaults = {...defaults, ...filter};
-        const queryParameters = filterToParams(filterWithDefaults);
-        const url = this.timeSeriesUrl(queryParameters);
+        const url = this.timeSeriesUrl(filterWithDefaults);
         if (url.toString().length <= MAX_URL_LENGTH) {
             const res = await this.webservice.getData<TimeSeriesResponse>(url.toString());
             return res.data;
@@ -170,8 +164,7 @@ export class PiWebserviceProvider {
             documentFormat: DocumentFormat.PI_JSON,
         }
         const filterWithDefaults = {...defaults, ...filter};
-        const queryParameters = filterToParams(filterWithDefaults);
-        const url = this.timeSeriesGridUrl(queryParameters);
+        const url = this.timeSeriesGridUrl(filterWithDefaults);
         const res = await this.webservice.getData<TimeSeriesResponse>(url.toString());
         return res.data;
     }
@@ -185,8 +178,7 @@ export class PiWebserviceProvider {
     async getScheduledTasks(filter: ScheduledTasksFilter): Promise<ScheduledTasksResponse> {
         const defaults: Partial<ScheduledTasksFilter> = {}
         const filterWithDefaults = {...defaults, ...filter}
-        const queryParameters = filterToParams(filterWithDefaults)
-        const url = this.scheduledTasksUrl(queryParameters)
+        const url = this.scheduledTasksUrl(filterWithDefaults)
         const res = await this.webservice.getData<ScheduledTasksResponse>(url.toString());
         return res.data;
     }
@@ -200,9 +192,7 @@ export class PiWebserviceProvider {
     async getTaskRuns(filter: TaskRunsFilter): Promise<TaskRunsResponse> {
         const defaults: Partial<TaskRunsFilter> = {}
         const filterWithDefaults = {...defaults, ...filter}
-        let queryParameters = filterToParams(filterWithDefaults)
-        queryParameters = queryParameters + "&documentFormat=PI_JSON"
-        const url = this.taskRunsUrl(queryParameters)
+        const url = this.taskRunsUrl(filterWithDefaults)
         const res = await this.webservice.getData<TaskRunsResponse>(url.toString());
         return res.data;
     }
@@ -216,8 +206,7 @@ export class PiWebserviceProvider {
     async getModuleRuntimes(filter: ModuleRuntimesFilter): Promise<ModuleRuntimesResponse> {
         const defaults: Partial<ModuleRuntimesFilter> = {}
         const filterWithDefaults = {...defaults, ...filter}
-        const queryParameters = filterToParams(filterWithDefaults)
-        const url = this.moduleRunTimesUrl(queryParameters)
+        const url = this.moduleRunTimesUrl(filterWithDefaults)
         const res = await this.webservice.getData<ModuleRuntimesResponse>(url.toString());
         return res.data;
     }
@@ -225,12 +214,13 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for locations request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @param useArchive whether to use the archive or not
      * @returns complete url for making a request
      */
-    locationsUrl(queryParameters: string, useArchive: boolean): URL {
+    locationsUrl(filter: LocationsFilter | ArchiveLocationsFilter, useArchive: boolean): URL {
         const path = useArchive ? 'archive/locations' : 'locations';
+        const queryParameters = filterToParams(filter)
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/${path}${queryParameters}`,
             this.baseUrl
@@ -240,10 +230,11 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for parameters request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @returns complete url for making a request
      */
-    parametersUrl(queryParameters: string): URL {
+    parametersUrl(filter: ParametersFilter): URL {
+        const queryParameters = filterToParams(filter)
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/archive/parameters${queryParameters}`,
             this.baseUrl
@@ -253,10 +244,11 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for attribute request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @returns complete url for making a request
      */
-    attributesUrl(queryParameters: string): URL {
+    attributesUrl(filter: AttributesFilter): URL {
+        const queryParameters = filterToParams(filter)
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/archive/attributes${queryParameters}`,
             this.baseUrl
@@ -266,10 +258,11 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for external forecast request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @returns complete url for making a request
      */
-    externalForecastsUrl(queryParameters: string): URL {
+    externalForecastsUrl(filter: ExternalForecastsFilter): URL {
+        const queryParameters = filterToParams(filter)
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/archive/netcdfstorageforecasts${queryParameters}`,
             this.baseUrl
@@ -279,10 +272,11 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for time series request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @returns complete url for making a request
      */
-    timeSeriesUrl(queryParameters: string): URL {
+    timeSeriesUrl(filter: TimeSeriesFilter): URL {
+        const queryParameters = filterToParams(filter)
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/timeseries${queryParameters}`,
             this.baseUrl
@@ -292,10 +286,11 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for time series grid request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @returns complete url for making a request
      */
-    timeSeriesGridUrl(queryParameters: string): URL {
+    timeSeriesGridUrl(filter: TimeSeriesGridFilter): URL {
+        const queryParameters = filterToParams(filter)
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/timeseries/grid${queryParameters}`,
             this.baseUrl
@@ -305,10 +300,11 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for scheduled tasks request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @returns complete url for making a request
      */
-    scheduledTasksUrl(queryParameters: string): URL {
+    scheduledTasksUrl(filter: ScheduledTasksFilter): URL {
+        const queryParameters = filterToParams(filter)
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/tasks/scheduled${queryParameters}`,
             this.baseUrl
@@ -318,10 +314,11 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for module run times request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @returns complete url for making a request
      */
-    moduleRunTimesUrl(queryParameters: string): URL {
+    moduleRunTimesUrl(filter: ModuleRuntimesFilter): URL {
+        const queryParameters = filterToParams(filter)
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/tasks/moduleruntimes${queryParameters}`,
             this.baseUrl
@@ -331,10 +328,13 @@ export class PiWebserviceProvider {
     /**
      * Construct URL for module run times request
      *
-     * @param queryParameters query string
+     * @param filter an object with request query parameters
      * @returns complete url for making a request
      */
-    taskRunsUrl(queryParameters: string): URL {
+    taskRunsUrl(filter: TaskRunsFilter): URL {
+        let queryParameters = filterToParams(filter)
+        // TODO: TaskRunsFilter should probably get a documentFormat member...
+        queryParameters = queryParameters + "&documentFormat=PI_JSON"
         return new URL(
             `${this.baseUrl.pathname}${this.API_ENDPOINT}/taskruns${queryParameters}`,
             this.baseUrl
