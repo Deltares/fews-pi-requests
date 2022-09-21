4b33056ed2c4da633237d06bb8654d047846a658
grijzea <andre.grijze@deltares.nl>
2022-09-21 17:07:27 +0200

    add types

--- src/response/index.ts
+++ src/response/index.ts
@@ -6,4 +6,5 @@ export * from './parameters'
 export * from './tasks'
 export * from './timeseries'
 export * from './importStatus'
+export * from './topology'
 
---
+++ src/response/topology/topologyNodeResponse.ts
@@ -0,0 +1,5 @@
+import { TopologyNode } from "./topologyNode";
+
+export interface TopologyNodeResponse {
+    topologyNodes: TopologyNode[];
+}
