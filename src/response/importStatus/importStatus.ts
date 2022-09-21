f6a34869e1b67b3e2affd3fdc3840cdcf15e79f3
grijzea <andre.grijze@deltares.nl>
2022-09-21 13:16:51 +0200

    added export and return type

--- src/response/importStatus/importStatus.ts
+++ src/response/importStatus/importStatus.ts
@@ -1,10 +1,10 @@
 export interface ImportStatus {
-    mcId?: string,
-    directory?: string,
-    dataFeed?: string,
-    lastImportTime?: string,
-    lastFileImported?: string,
-    fileRead?: number,
-    fileFailed?: number,
-    lastImportTimeBackgroundColor?: string,
+    mcId?: string;
+    directory?: string;
+    dataFeed?: string;
+    lastImportTime?: string;
+    lastFileImported?: string;
+    fileRead?: number;
+    fileFailed?: number;
+    lastImportTimeBackgroundColor?: string;
 }
--- src/response/importStatus/index.ts
+++ src/response/importStatus/index.ts
@@ -1,2 +1,2 @@
 export * from './importStatusResponse'
-export * from './importStatusResponse'
+export * from './importStatus'
