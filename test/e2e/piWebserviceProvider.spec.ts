8bf7c43d060eb22799c6c46dac2d043b0216c189
Anne Markensteijn <anne.markensteijn@vortech.nl>
2023-04-12 12:55:58 +0200

    Fix e2e test
    Use existing workflowId.

--- test/e2e/piWebserviceProvider.spec.ts
+++ test/e2e/piWebserviceProvider.spec.ts
@@ -19,7 +19,7 @@ describe("pi webservice provider", function () {
         expect(res.locations.length).toBeGreaterThan(0);
     })
 
-    it("gets called when done", async function () {
+    it("get timeseries with relative url", async function () {
 
         const provider = new PiWebserviceProvider(baseUrl);
         const res = await provider.getTimeSeriesWithRelativeUrl("rest/fewspiservice/v1/timeseries?timeSeriesType=EXTERNAL_HISTORICAL&locationIds=Lith_beneden&parameterIds=H.meting&documentFormat=PI_JSON");
@@ -30,7 +30,7 @@ describe("pi webservice provider", function () {
             expect(timeSeries?.header?.locationId).toBe("Lith_beneden")
         }
     })
-    it("gets called when done", async function () {
+    it("get display groups info", async function () {
 
         const provider = new PiWebserviceProvider(baseUrl);
         const filter = {} as DisplayGroupsFilter;
@@ -40,7 +40,7 @@ describe("pi webservice provider", function () {
 
     })
 
-    it("gets called when done", async function () {
+    it("get topology nodes", async function () {
 
         const provider = new PiWebserviceProvider(baseUrl);
         const res = await provider.getTopologyNodes();
@@ -50,21 +50,21 @@ describe("pi webservice provider", function () {
 
     })
 
-    it("gets called when done", async function () {
+    it("get completed task runs", async function () {
 
         const provider = new PiWebserviceProvider(baseUrl);
         const taskRunFilter: TaskRunsFilter = {
             taskRunStatusIds: ["A"],
             onlyForecasts: false,
             documentFormat: DocumentFormat.PI_JSON,
-            workflowId: "Radar_Nowcast_Det",
+            workflowId: "Plateau3_KGT",
             onlyCurrent: true
         };
         const res = await provider.getTaskRuns(taskRunFilter);
         expect(res.taskRuns.length).toBeGreaterThan(0);
         expect(res.taskRuns[0].current).toBe(true);
         for (const taskRun of res.taskRuns) {
-            expect(taskRun.workflowId).toBe("Radar_Nowcast_Det")
+            expect(taskRun.workflowId).toBe("Plateau3_KGT")
         }
 
     });
