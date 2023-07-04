14524398141a504821ff6996d42a1f46a7e2051d
ekkelenk <rudie.ekkelenkamp@deltares.nl>
2023-07-04 13:10:50 +0200

    Added background color and foreground color to flags response.

--- test/unit/fews/flags.spec.ts
+++ test/unit/fews/flags.spec.ts
@@ -4,7 +4,6 @@ import 'cross-fetch/polyfill';
 import fetchMock from "fetch-mock";
 import expectedFlagsResponse from '../mock/flags.json'
 
-
 describe("/flags", function () {
 
     afterAll(function () {
@@ -18,11 +17,14 @@ describe("/flags", function () {
         });
 
         const provider = new PiWebserviceProvider("https://mock.dev/fewswebservices")
-
         const response = await provider.getFlags();
         expect(response).toStrictEqual(expectedFlagsResponse);
         expect(response.flags?.length).toBe(14)
         if (response.flags && response.flags.length == 14) {
+            expect(response.flags[0].flag).toBe("0")
+            expect(response.flags[0].source).toBe("ORIGINAL")
+            expect(response.flags[0].quality).toBe("RELIABLE")
+            expect(response.flags[0].name).toBe( "original reliable")
             expect(response.flags[0].backgroundColor).toBe('#FFFFFF')
             expect(response.flags[0].foregroundColor).toBe('#000000')
         }
