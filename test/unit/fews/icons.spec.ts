a1a1e89e075b93ab87bf7f69583d91db3e05ec9e
Anne Markensteijn <anne.markensteijn@vortech.nl>
2024-03-29 08:43:58 +0100

    Split icon resource unit tests over it calls

--- test/unit/fews/icons.spec.ts
+++ test/unit/fews/icons.spec.ts
@@ -13,15 +13,24 @@ describe("icon resources", function () {
         const provider = new PiWebserviceProvider("https://mock.dev/fewswebservices")
         const results = await provider.resourcesIconsUrl('meteo_site_data.gif')
         expect(results.toString()).toStrictEqual("https://mock.dev/fewswebservices/rest/fewspiservice/v1/resources/icons/meteo_site_data.gif");
-
-        const resultsAbsolute = await provider.resourcesIconsUrl('https://mydomain.com/css/style.css')
-        expect(resultsAbsolute.toString()).toStrictEqual("https://mydomain.com/css/style.css");
-
-        const resultsHttpsPath = await provider.resourcesIconsUrl('https/css/style.css')
-        expect(resultsHttpsPath.toString()).toStrictEqual("https://mock.dev/fewswebservices/rest/fewspiservice/v1/resources/icons/https/css/style.css");
-
-        const resultsHttpPath = await provider.resourcesIconsUrl('http/css/style.css')
-        expect(resultsHttpPath.toString()).toStrictEqual("https://mock.dev/fewswebservices/rest/fewspiservice/v1/resources/icons/http/css/style.css");
-
     })
+
+    it("get icon resource with absolute path", async function () {
+        const provider = new PiWebserviceProvider("https://mock.dev/fewswebservices")
+        const resultsAbsolute = await provider.resourcesIconsUrl('https://mydomain.com/images/icon.png')
+        expect(resultsAbsolute.toString()).toStrictEqual("https://mydomain.com/images/icon.png");
+    })
+
+    it("get icon resource with https in path", async function() {
+        const provider = new PiWebserviceProvider("https://mock.dev/fewswebservices")
+        const resultsHttpsPath = await provider.resourcesIconsUrl('https/images/icon.png')
+        expect(resultsHttpsPath.toString()).toStrictEqual("https://mock.dev/fewswebservices/rest/fewspiservice/v1/resources/icons/https/images/icon.png");
+    })
+
+    it("get icon resource with http in path", async function() {
+        const provider = new PiWebserviceProvider("https://mock.dev/fewswebservices")
+        const resultsHttpPath = await provider.resourcesIconsUrl('http/images/icon.png')
+        expect(resultsHttpPath.toString()).toStrictEqual("https://mock.dev/fewswebservices/rest/fewspiservice/v1/resources/icons/http/images/icon.png");
+    })
+
 })
