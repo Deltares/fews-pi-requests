af0473efe7026583118b4c41f1ffe4bf4bf2ff6b
grijzea <andre.grijze@deltares.nl>
2024-04-16 09:28:00 +0200

    add showParentLocation and attributeIds

--- src/requestParameters/locationsFilter.ts
+++ src/requestParameters/locationsFilter.ts
@@ -3,7 +3,9 @@ import type { BaseFilter } from "./baseFilter";
 export interface LocationsFilter extends BaseFilter {
     filterId?: string;
     showAttributes?: boolean;
+    showParentLocations?: boolean;
     includeLocationRelations?: boolean;
     includeTimeDependency?: boolean;
     includeIconNames?: boolean;
+    attributeIds?: string | string[];
 }
