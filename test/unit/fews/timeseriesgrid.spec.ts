a0ff319e18ac6c2c98289c546d3fe280c04e6a56
Cees Voesenek <cees.voesenek@vortech.nl>
2024-06-11 16:03:39 +0200

    Clarify test suite/test name

--- test/unit/fews/timeseriesgrid.spec.ts
+++ test/unit/fews/timeseriesgrid.spec.ts
@@ -7,13 +7,13 @@ import {TimeSeriesGridFilter} from "../../../src/requestParameters/timeSeriesGri
 import {DocumentFormat} from "../../../src";
 
 
-describe("archive/locations", function () {
+describe("timeseries/grid", function () {
 
     afterAll(function () {
         fetchMock.restore();
     });
 
-    it("gets called when done", async function () {
+    it("gets a time series for a WMS layer", async function () {
         fetchMock.get("https://mock.dev/fewswebservices/rest/fewspiservice/v1/timeseries/grid?documentFormat=PI_JSON&layers=-knmi_harmonie40--air_pressure_fixed_height-&x=469168.1587130217&y=6773416.739869252&startTime=2020-12-22T21%3A00%3A00.000Z&endTime=2020-12-22T21%3A00%3A00.000Z&bbox=233081.51067565128%2C6437014.8860724205%2C1028684.8666990183%2C6955886.640000704&importFromExternalDataSource=true&externalForecastTime=2020-12-20T21%3A00%3A00.000Z", {
             status: 200,
             body: JSON.stringify(expectedResponse)
