44148b0ecb40e7d07eeb2ee2a34308c9baca0da6
Cees Voesenek <cees.voesenek@vortech.nl>
2025-02-25 15:33:47 +0100

    Fix splitting URLs by a search parameter that is not first in the list

--- src/utils/duplicates.ts
+++ src/utils/duplicates.ts
@@ -1,14 +1,8 @@
 export function duplicates(keys: string[]): Map<string, number> {
     const duplicates = new Map<string, number>()
-    const seen = new Set<string>();
-    for (const key of keys) {
-        if (seen.has(key)) {
-            let frequency = duplicates.get(key)
-            if (frequency === undefined) frequency = 0 
-            duplicates.set(key, frequency++ )
-        } else {
-            seen.add(key)
-        }
-    }
+    keys.forEach(key => {
+        const frequency = duplicates.get(key) ?? 0
+        duplicates.set(key, frequency + 1)
+    })
     return duplicates
 }
