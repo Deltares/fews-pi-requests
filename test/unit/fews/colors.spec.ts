19d4acef5369e94677106767e0adb8bd42967164
cees-wolfs-vt <cees.wolfs@vortech.nl>
2025-05-01 09:34:06 +0200

    Fix test

--- test/unit/fews/colors.spec.ts
+++ test/unit/fews/colors.spec.ts
@@ -11,7 +11,7 @@ describe("/colors", function () {
 
   it("test colors", async function () {
     fetchMock.get(
-      "https://mock.dev/fewswebservices/rest/fewspiservice/v1/colors",
+      "https://mock.dev/fewswebservices/rest/fewspiservice/v1/colors/default",
       {
         status: 200,
         body: JSON.stringify(expectedColorsResponse),
