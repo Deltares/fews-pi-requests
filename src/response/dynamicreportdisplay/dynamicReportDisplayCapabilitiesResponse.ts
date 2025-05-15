16fb284d5bf94a4fda02020092e6e2b068f21955
cees-wolfs-vt <cees.wolfs@vortech.nl>
2025-05-15 09:23:26 +0200

    Update types for capability respsonse

--- src/response/dynamicreportdisplay/dynamicReportDisplayCapabilitiesResponse.ts
+++ src/response/dynamicreportdisplay/dynamicReportDisplayCapabilitiesResponse.ts
@@ -1,14 +1,25 @@
+interface DimensionWithPeriod {
+  name: string;
+  units: string;
+  default: string;
+  period: string;
+}
+
+interface DimensionWithTimes {
+  name: string;
+  units: string;
+  default: string;
+  times: string;
+}
+
+export type Dimension = DimensionWithPeriod | DimensionWithTimes;
+
 export interface DynamicReportDisplayCapabilitiesResponse {
   /**
    * DynamicReportDisplayCapabilities
    */
   dynamicReportDisplayCapabilities: {
-    dimension?: {
-      name: string;
-      units: string;
-      default: string;
-      period: string;
-    };
+    dimension?: Dimension;
     selectableLocations?: [
       {
         id: string;
