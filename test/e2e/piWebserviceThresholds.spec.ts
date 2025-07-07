c3189cd37a1ec9bf1334289c6e8e6156d286783a
Rudie Ekkelenkamp <rudie.ekkelenkamp@deltares.nl>
2025-07-07 14:02:18 +0200

    test thresholds.

---
+++ test/e2e/piWebserviceThresholds.spec.ts
@@ -0,0 +1,35 @@
+import 'cross-fetch/polyfill';
+import {
+    PiWebserviceProvider,
+} from "../../src";
+import {TopologyThresholdFilter} from "../../lib/types";
+
+const baseUrl = process.env.DOCKER_URL || "";
+
+describe("pi webservice thresholds", function () {
+
+    it("get all topology thresholds", async function () {
+        const provider = new PiWebserviceProvider(baseUrl);
+        const filter: TopologyThresholdFilter = {
+        }
+        const res= await provider.getTopologyThresholds(filter);
+        expect(res?.topologyNodes?.length).toBeGreaterThan(9);
+
+        const topologyNode1 = res.topologyNodes.find(item => item.id === 'viewer_rivers_level_stations');
+        expect(topologyNode1?.filterLocationsCount).toEqual(4);
+
+        const topologyNode2 = res.topologyNodes.find(item => item.id === 'viewer_rivers_critical_points');
+        expect(topologyNode2?.filterLocationsCount).toEqual(8);
+
+    })
+
+    it("get topology thresholds by nodeId", async function () {
+        const provider = new PiWebserviceProvider(baseUrl);
+        const filter: TopologyThresholdFilter = {
+            nodeId: 'viewer_rivers'
+        }
+        const res= await provider.getTopologyThresholds(filter);
+        expect(res?.topologyNodes?.length).toBeGreaterThan(0);
+        expect(res.topologyNodes[0].filterLocationsCount).toEqual(12);
+    })
+})
