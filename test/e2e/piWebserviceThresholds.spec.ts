a6936e119f3c423b15a0e9d1fdfc7096a472048c
Rudie Ekkelenkamp <rudie.ekkelenkamp@deltares.nl>
2025-07-08 13:09:26 +0200

    E2E testing using docker container with test config.

--- test/e2e/piWebserviceThresholds.spec.ts
+++ test/e2e/piWebserviceThresholds.spec.ts
@@ -6,14 +6,14 @@ import {
 
 
 const baseUrl = process.env.DOCKER_URL || "";
+const fewsVersion: number = process.env.FEWS_VERSION ? parseInt(process.env.FEWS_VERSION, 10) : 999999;
 
 describe("pi webservice thresholds", function () {
 
     it("get all topology thresholds", async function () {
         const provider = new PiWebserviceProvider(baseUrl);
-        const filter: TopologyThresholdFilter = {
-        }
-        const res= await provider.getTopologyThresholds(filter);
+        const filter: TopologyThresholdFilter = {}
+        const res = await provider.getTopologyThresholds(filter);
         expect(res?.topologyNodes?.length).toBeGreaterThan(9);
 
         const topologyNode1 = res.topologyNodes.find(item => item.id === 'viewer_rivers_level_stations');
