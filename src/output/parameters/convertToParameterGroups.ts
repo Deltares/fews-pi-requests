fcbdcd50b18a57332c7810e5bd3389c7fd2decf7
Werner Kramer <werner.kramer@vortech.nl>
2025-11-11 22:52:06 +0100

    refactor: improve code formatting and type annotations in convertToParameterGroups.ts

--- src/output/parameters/convertToParameterGroups.ts
+++ src/output/parameters/convertToParameterGroups.ts
@@ -1,7 +1,7 @@
-import type { TimeSeriesParametersResponse } from "../../response/timeseriesparameters/timeSeriesParametersResponse"
-import type { ParameterGroupsOutput } from "./parameterGroupsOutput"
-import type { Parameter } from "./parameter"
-import type { ParameterGroup } from "./parameterGroup"
+import type { TimeSeriesParametersResponse } from '../../response/timeseriesparameters/timeSeriesParametersResponse'
+import type { ParameterGroupsOutput } from './parameterGroupsOutput'
+import type { Parameter } from './parameter'
+import type { ParameterGroup } from './parameterGroup'
 
 /**
  * Checks if the provided value is of type Parameter.
@@ -9,7 +9,7 @@ import type { ParameterGroup } from "./parameterGroup"
  * @param {Parameter | any} parameter - The value to be checked.
  * @returns {boolean} True if the value is of type Parameter, false otherwise.
  */
-function isParameter(parameter: Parameter | any): parameter is Parameter {
+function isParameter(parameter: Parameter | unknown): parameter is Parameter {
   return (parameter as Parameter).name !== undefined
 }
 
@@ -19,7 +19,9 @@ function isParameter(parameter: Parameter | any): parameter is Parameter {
  * @param {ParameterGroup | any} parameter - The value to be checked.
  * @returns {boolean} True if the value is of type ParameterGroup, false otherwise.
  */
-function isParameterGroup(parameter: ParameterGroup | any): parameter is ParameterGroup {
+function isParameterGroup(
+  parameter: ParameterGroup | unknown,
+): parameter is ParameterGroup {
   return (parameter as ParameterGroup).parameters !== undefined
 }
 
@@ -29,38 +31,46 @@ function isParameterGroup(parameter: ParameterGroup | any): parameter is Paramet
  * @param {TimeSeriesParametersResponse} response - The response from the TimeSeriesParameters API call.
  * @returns {ParameterGroupsOutput} The converted output in the ParameterGroupsOutput format.
  */
-export function convertToParameterGroups(response: TimeSeriesParametersResponse): ParameterGroupsOutput {
+export function convertToParameterGroups(
+  response: TimeSeriesParametersResponse,
+): ParameterGroupsOutput {
   const groupIds: string[] = []
   const result: ParameterGroupsOutput = {
-    parameters: []
+    parameters: [],
   }
-  for ( const tsParameter of response.timeSeriesParameters) {
+  for (const tsParameter of response.timeSeriesParameters) {
     const parameterGroupId = tsParameter.parameterGroup
     if (parameterGroupId !== undefined && groupIds.includes(parameterGroupId)) {
-      const group = result.parameters.find( g => "parameters" in g && g.id === parameterGroupId )
-      if ( isParameterGroup(group) && isParameter(tsParameter)) {
+      const group = result.parameters.find(
+        (g) => 'parameters' in g && g.id === parameterGroupId,
+      )
+      if (isParameterGroup(group) && isParameter(tsParameter)) {
         const parameter: Parameter = {
           id: tsParameter.id,
           name: tsParameter.name,
-          shortName: tsParameter.shortName
+          shortName: tsParameter.shortName,
         }
         group.parameters.push(parameter)
       }
-    } else if(parameterGroupId !== undefined) {
+    } else if (parameterGroupId !== undefined) {
       groupIds.push(parameterGroupId)
       const parameterGroup: ParameterGroup = {
         id: parameterGroupId,
-        parameterType: tsParameter.parameterType === "instantaneous" ? "instantaneous" : "accumulative",
+        parameterType:
+          tsParameter.parameterType === 'instantaneous'
+            ? 'instantaneous'
+            : 'accumulative',
         unit: tsParameter.unit ?? '',
-        parameters: []
+        parameters: [],
       }
-      if ( tsParameter.displayUnit !== tsParameter.unit) parameterGroup.displayUnit = tsParameter.displayUnit
-      if ( tsParameter.usesDatum === "true") parameterGroup.usesDatum = true
+      if (tsParameter.displayUnit !== tsParameter.unit)
+        parameterGroup.displayUnit = tsParameter.displayUnit
+      if (tsParameter.usesDatum === 'true') parameterGroup.usesDatum = true
       if (isParameter(tsParameter)) {
         const parameter: Parameter = {
           id: tsParameter.id,
           name: tsParameter.name,
-          shortName: tsParameter.shortName
+          shortName: tsParameter.shortName,
         }
         parameterGroup.parameters.push(parameter)
       }
@@ -69,7 +79,7 @@ export function convertToParameterGroups(response: TimeSeriesParametersResponse)
       const parameter: Parameter = {
         id: tsParameter.id,
         name: tsParameter.name,
-        shortName: tsParameter.shortName
+        shortName: tsParameter.shortName,
       }
       result.parameters.push(parameter)
     }
