a3316975d9a02cd252eb34dee66a74fc77ec67df
Werner Kramer <werner.kramer@vortech.nl>
2025-11-11 22:49:49 +0100

    refactor: improve code formatting and consistency in reports.spec.ts

--- test/unit/fews/reports.spec.ts
+++ test/unit/fews/reports.spec.ts
@@ -1,41 +1,46 @@
-import {PiWebserviceProvider} from '../../../src/piWebserviceProvider'
+import { PiWebserviceProvider } from '../../../src/piWebserviceProvider'
 
-import fetchMock from "fetch-mock";
-import {ReportsFilter, RunTaskFilter} from '../../../src/requestParameters';
+import fetchMock from 'fetch-mock'
+import { ReportsFilter } from '../../../src/requestParameters'
 import expectedResponse from '../mock/reports.json'
 
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from 'vitest'
 
-describe("reports", function () {    it("gets called when done", async function () {
-        fetchMock.get("https://mock.dev/fewswebservices/rest/fewspiservice/v1/reports?moduleInstanceIds=GenerateReport", {
-            status: 200,
-            body: JSON.stringify(expectedResponse)
-        });
+describe('reports', function () {
+  it('gets called when done', async function () {
+    fetchMock.get(
+      'https://mock.dev/fewswebservices/rest/fewspiservice/v1/reports?moduleInstanceIds=GenerateReport',
+      {
+        status: 200,
+        body: JSON.stringify(expectedResponse),
+      },
+    )
 
-        const provider = new PiWebserviceProvider("https://mock.dev/fewswebservices")
+    const provider = new PiWebserviceProvider(
+      'https://mock.dev/fewswebservices',
+    )
 
-        const filter: ReportsFilter = {
-            moduleInstanceIds: "GenerateReport"
-        }
-        const results = await provider.getReports(filter);
-        expect(results).toStrictEqual(expectedResponse);
-        expect("reports" in results).toBe(true);
-        expect(results?.reports?.length).toBe(1);
-        if (results?.reports?.length === 1) {
-            const report = results.reports[0]
-            expect(report?.moduleInstanceId).toBe("GenerateReport")
-            expect(report?.moduleInstanceName).toBe("GenerateReportName")
-            expect(report?.mimeType).toBe("application/pdf")
-            expect(report?.items?.length).toBe(1);
-            if (report?.items.length === 1) {
-                const reportDetails = report.items[0]
-                expect(reportDetails?.moduleInstanceId).toBe("GenerateReport")
-                expect(reportDetails?.taskRunId).toBe("taskRunId1")
-                expect(reportDetails?.reportId).toBe(1)
-                expect(reportDetails?.timeZero).toBe("1970-01-20T21:33:40Z")
-                expect(reportDetails?.isCurrent).toBe(true)
-            }
-        }
-
-    });
-});
+    const filter: ReportsFilter = {
+      moduleInstanceIds: 'GenerateReport',
+    }
+    const results = await provider.getReports(filter)
+    expect(results).toStrictEqual(expectedResponse)
+    expect('reports' in results).toBe(true)
+    expect(results?.reports?.length).toBe(1)
+    if (results?.reports?.length === 1) {
+      const report = results.reports[0]
+      expect(report?.moduleInstanceId).toBe('GenerateReport')
+      expect(report?.moduleInstanceName).toBe('GenerateReportName')
+      expect(report?.mimeType).toBe('application/pdf')
+      expect(report?.items?.length).toBe(1)
+      if (report?.items.length === 1) {
+        const reportDetails = report.items[0]
+        expect(reportDetails?.moduleInstanceId).toBe('GenerateReport')
+        expect(reportDetails?.taskRunId).toBe('taskRunId1')
+        expect(reportDetails?.reportId).toBe(1)
+        expect(reportDetails?.timeZero).toBe('1970-01-20T21:33:40Z')
+        expect(reportDetails?.isCurrent).toBe(true)
+      }
+    }
+  })
+})
