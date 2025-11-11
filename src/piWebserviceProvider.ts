5b3d9517557fa3e83363aea846dcd1335a0160f0
Werner Kramer <werner.kramer@vortech.nl>
2025-11-11 22:53:22 +0100

    refactor: remove unused ESLint directive in getParameters method

--- src/piWebserviceProvider.ts
+++ src/piWebserviceProvider.ts
@@ -183,7 +183,6 @@ export class PiWebserviceProvider {
      */
     async getParameters(filter: ParametersFilter): Promise<TimeSeriesParametersResponse>
     async getParameters(filter: ParametersFilter, output?: ParameterGroupsOutputOptions): Promise<ParameterGroupsOutput>
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async getParameters(filter: ParametersFilter, output?: ParameterOutputOptions): Promise<ParameterGroupsOutput|TimeSeriesParametersResponse> {
         const url = this.parametersUrl(filter);
         const res = await this.webservice.getData<TimeSeriesParametersResponse>(url.toString());
