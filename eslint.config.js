bf089c595908d22e9c5ff50eb618e61832d6e874
Werner Kramer <werner.kramer@deltares.nl>
2025-11-14 08:28:52 +0100

    refactor: update parserOptions to allow TypeScript files in ESLint configuration

--- eslint.config.js
+++ eslint.config.js
@@ -13,7 +13,7 @@ export default defineConfig(
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
-        projectService: { allowDefaultProject: ['*.js'] },
+        projectService: { allowDefaultProject: ['*.js', '*.ts'] },
       },
     },
   },
